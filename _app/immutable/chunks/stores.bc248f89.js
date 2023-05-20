1b9ec677c21d251882f50f31885329ba2a0a516b
funkymonkeymonk <funkymonkeymonk@users.noreply.github.com>
2023-05-20 02:00:05 +0000

    Deploying to gh-pages from @ MeepleProfessionals/homepage@f56bafd48b3eaaf45f23b8f5e780c36a28e10916 🚀

--- _app/immutable/chunks/stores.bc248f89.js
+++
@@ -1,1 +0,0 @@
-import"./index.f30d4c5b.js";import{s as e}from"./singletons.fb4f6a50.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
