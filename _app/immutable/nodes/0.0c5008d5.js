cf10a30fb390242f6ed33051f5d828619aeca954
funkymonkeymonk <funkymonkeymonk@users.noreply.github.com>
2023-05-20 01:27:06 +0000

    Deploying to gh-pages from @ MeepleProfessionals/homepage@866cda2ae46845ccd8dfb4467cac27c2993aa0c8 🚀

---
+++ _app/immutable/nodes/0.0c5008d5.js
@@ -0,0 +1,15 @@
+import{S as $,i as v,s as w,k as d,q as b,C as D,l as f,m,h as l,r as W,D as S,n as L,E as u,F as p,G as j,H as C,I as E,y as G,a as I,z as T,c as Y,A as q,b as g,J as P,K as z,L as A,g as h,d as y,B as O}from"../chunks/index.f30d4c5b.js";import{p as R}from"../chunks/stores.bc248f89.js";const k=!0,x=!1,N=Object.freeze(Object.defineProperty({__proto__:null,csr:x,prerender:k},Symbol.toStringTag,{value:"Module"}));const{document:_}=C;function B(o){let e,c,a,n;return{c(){e=d("script"),a=d("script"),n=b(`window.dataLayer = window.dataLayer || []
+  
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+  
+      gtag('js', new Date())
+      gtag('config', 'G-7YD09L7W4W')`),this.h()},l(i){const s=D("svelte-1mn7uci",_.head);e=f(s,"SCRIPT",{src:!0});var t=m(e);t.forEach(l),a=f(s,"SCRIPT",{});var r=m(a);n=W(r,`window.dataLayer = window.dataLayer || []
+  
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+  
+      gtag('js', new Date())
+      gtag('config', 'G-7YD09L7W4W')`),r.forEach(l),s.forEach(l),this.h()},h(){e.async=!0,S(e.src,c="https://www.googletagmanager.com/gtag/js?id=G-7YD09L7W4W")||L(e,"src",c)},m(i,s){u(_.head,e),u(_.head,a),u(a,n)},p,i:p,o:p,d(i){l(e),l(a)}}}function F(o,e,c){let a;return j(o,R,n=>c(0,a=n)),o.$$.update=()=>{o.$$.dirty&1&&typeof gtag<"u"&&gtag("config","G-7YD09L7W4W",{page_title:document.title,page_path:a.url.pathname})},[a]}class H extends ${constructor(e){super(),v(this,e,F,B,w,{})}}function J(o){let e,c,a,n;e=new H({});const i=o[1].default,s=E(i,o,o[0],null);return{c(){G(e.$$.fragment),c=I(),a=d("div"),s&&s.c(),this.h()},l(t){T(e.$$.fragment,t),c=Y(t),a=f(t,"DIV",{class:!0});var r=m(a);s&&s.l(r),r.forEach(l),this.h()},h(){L(a,"class","app svelte-a08u1e")},m(t,r){q(e,t,r),g(t,c,r),g(t,a,r),s&&s.m(a,null),n=!0},p(t,[r]){s&&s.p&&(!n||r&1)&&P(s,i,t,t[0],n?A(i,t[0],r,null):z(t[0]),null)},i(t){n||(h(e.$$.fragment,t),h(s,t),n=!0)},o(t){y(e.$$.fragment,t),y(s,t),n=!1},d(t){O(e,t),t&&l(c),t&&l(a),s&&s.d(t)}}}function K(o,e,c){let{$$slots:a={},$$scope:n}=e;return o.$$set=i=>{"$$scope"in i&&c(0,n=i.$$scope)},[n,a]}class Q extends ${constructor(e){super(),v(this,e,K,J,w,{})}}export{Q as component,N as universal};
